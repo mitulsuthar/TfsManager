74aa5db13408a869b1d4ae63b3701470a974c9d3
Mitul Suthar <gitHubCoder4[removethis]@gmail.com>
2014-09-19 22:27:55 -0400

    Resovled installation bug

--- tools/InstallTfsManager.ps1
+++ tools/InstallTfsManager.ps1
@@ -1,19 +1,24 @@
 # based off of the scrit at http://psget.net/GetPsGet.ps1
 # based2 off of the script at https://github.com/ligershark/psbuild/blob/master/src/GetPSBuild.ps1
 function Install-TfsManager {
-    $ModulePaths = @($Env:PSModulePath -split ';')
-    
-    $ExpectedUserModulePath = Join-Path -Path ([Environment]::GetFolderPath('MyDocuments')) -ChildPath WindowsPowerShell\Modules
-    $Destination = $ModulePaths | Where-Object { $_ -eq $ExpectedUserModulePath}
-    if (-not $Destination) {
-        $Destination = $ModulePaths | Select-Object -Index 0
+[cmdletbinding()]
+param()
+    $ExpectedUserModulePath = Join-Path -Path ([Environment]::GetFolderPath('MyDocuments')) -ChildPath WindowsPowerShell\Modules    
+    $Destination = $ExpectedUserModulePath
+    $destFile = (join-path $Destination "\TFsManager.Cmdlets\TfsManager.Cmdlets.dll")
+    $destFile    
+    $destFolder = ([System.IO.Path]::GetDirectoryName($destFile))
+     if(!(test-path $destFolder)){
+        new-item -path $destFolder -ItemType Directory | out-null
     }
-
+    $destFolder
     $downloadUrl = 'https://github.com/mitulsuthar/TfsManager/raw/master/lib/TFSManager.Cmdlets.dll'
     New-Item ($Destination + "\TfsManager.Cmdlets\") -ItemType Directory -Force | out-null
     'Downloading TfsManager from {0}' -f $downloadUrl | Write-Host
     $client = (New-Object Net.WebClient)
     $client.Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
+    Write-Verbose -Message $downloadUrl
+    Write-Verbose -Message $Destination
     $client.DownloadFile($downloadUrl, $Destination + "\TfsManager.Cmdlets\TfsManager.Cmdlets.dll")
 
     $executionPolicy  = (Get-ExecutionPolicy)
@@ -39,4 +44,4 @@ For more information execute:
     Write-Host "TfsManager cmdlets are installed and ready to use" -Foreground Green    
 }
 
-Install-TfsManager
+Install-TfsManager -Verbose
